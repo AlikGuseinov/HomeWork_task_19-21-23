1c164422950694e5c20e913742434fa134dd5928
AlikG <9774952689@mail.ru>
2023-04-27 21:10:19 +0300

    Added task

--- Program.cs
+++ Program.cs
@@ -1,4 +1,75 @@
-﻿Console.Write("Введите координату (X) первой точки: ");
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+/*
+
+Console.WriteLine("Задача №23");
+Console.Write("Введите число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+for (int b = 0; b <= a; b++)
+{
+
+    int c =b*b*b;
+
+    Console.WriteLine(c);      
+}
+*/
+
+// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+/*
+
+Console.WriteLine("Задача №19");
+
+Console.Write("Введите пятизначное число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+string number = Convert.ToString(a);
+
+
+
+    if (number[0]==number[4] || number[1]==number[3])
+
+    {
+
+        Console.WriteLine($"Число: {a} является пилндромом");
+
+    }
+
+    else
+
+    {
+
+        Console.WriteLine($"Число: {a} не является палиндромом");
+
+    }
+
+
+
+if (number.Length > 5 && number.Length < 5)
+
+{
+
+    Console.WriteLine("Вы ввели не верное число для проверки на полиндром");
+
+}
+*/
+
+// Задача №21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+/*
+Console.WriteLine("Задача №21");
+Console.Write("Введите координату (X) первой точки: ");
 
 double x1 = Convert.ToDouble(Console.ReadLine());
 
@@ -47,3 +118,4 @@ double z2 = Convert.ToDouble(Console.ReadLine());
 
 
  Console.WriteLine("Расстояние между введенными координатами: "+Final);
+ */
