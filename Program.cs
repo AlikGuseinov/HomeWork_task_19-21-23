0618b9864e4e25c8d8a3ccae40ea919a429b59c6
AlikG <9774952689@mail.ru>
2023-04-27 20:59:36 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿Console.Write("Введите координату (X) первой точки: ");
+
+double x1 = Convert.ToDouble(Console.ReadLine());
+
+
+
+Console.Write("Введите координату (У) первой точки: ");
+
+double y1 = Convert.ToDouble(Console.ReadLine());
+
+
+
+Console.Write("Введите координату (Z) первой точки: ");
+
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+
+
+Console.WriteLine();
+
+Console.WriteLine();
+
+
+
+Console.Write("Введите координату (X) второй точки: ");
+
+double x2 = Convert.ToDouble(Console.ReadLine());
+
+
+
+Console.Write("Введите координату (У) второй точки: ");
+
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+
+
+Console.Write("Введите координату (Z) второй точки: ");
+
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+
+
+
+
+ double Final = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1)*(y2 - y1)) + ((z2 - z1)*(z2 - z1)));
+
+
+
+ Console.WriteLine("Расстояние между введенными координатами: "+Final);
